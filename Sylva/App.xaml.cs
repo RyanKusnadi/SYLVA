31bfbca967bf64d67f3a3007584f16ed293475f6
RyanKusnadi <ryankusnadi2002@mail.ugm.ac.id>
2021-04-18 18:08:16 +0700

    Add project files.

---
+++ Sylva/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Sylva.Services;
+using Sylva.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sylva
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Sylva/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Sylva.Models;
+using Sylva.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sylva.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
